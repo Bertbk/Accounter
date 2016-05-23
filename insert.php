fdea459e7ce7cccdf17d45bdb5ad7265d4606daa
BThierry <bertand.thierry1@gmail.com>
2016-05-23 15:11:34 +0200

    avoid doubling

--- insert.php
+++ insert.php
@@ -18,9 +18,23 @@ catch (Exception $e)
 
 if(isset($_POST['submit_sha']))
 {
+	try{
+	$isok = $db->exec('INSERT INTO test_sha(id, sha) VALUES(NULL, '.(int)htmlspecialchars($_POST['sha_to_sql']).')');
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}
 	$sha_to_sql = htmlspecialchars($_POST['sha_to_sql']);
-  	 echo '<p> coucou : '.$sha_to_sql.'</p>';
-	 
+	if($isok)
+	{
+  	 echo '<p> Ajouté dans la BDD : '.$sha_to_sql.'</p>';
+	}
+	 else
+	 {
+  	 echo '<p>'.$sha_to_sql.' is already in the db</p>';		 
+	 }
+		 
 }
 ?>
 
