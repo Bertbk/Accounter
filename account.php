e7152548428f44d4f265fc36193ab20b2ac1ca65
BThierry <bertand.thierry1@gmail.com>
2016-05-24 13:35:10 +0200

    account.php

---
+++ account.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<script>
+function validate() {
+    var x;
+    x = document.getElementById("sha_to_sql_value").value;
+    if (x == "") {
+        alert("Enter a Valid SHA number");
+        return false;
+    };
+
+	if (isNaN(parseInt(x)) || !isFinite(x)) {
+//        alert("Enter a Valid SHA number");
+//        return false;
+    };
+}
+</script>
+</head>
+<body>
+
+<?php
+$sha_url = "";
+empty($_GET['sha']) ? $sha_url = "" : $sha_url = htmlspecialchars($_GET['sha']);
+echo '<p>SHA FROM URL= '.$sha_url.'</p>'
+?>
+
+<?php 
+// on se connecte à MySQL 
+try
+{
+$db = new PDO('mysql:host=localhost;dbname=dividethebill;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+echo '<h1>SHA = GOOD ? </h1>';
+
+
+try
+{
+	$myquery = 'SELECT * FROM accounts WHERE hashid = :sha_url OR hashid_admin = :sha_url';
+	$prepare_query = $db->prepare($myquery);
+	$prepare_query->bindValue(':sha_url', $sha_url, PDO::PARAM_STR);
+	$prepare_query->execute();
+	$reponse = $prepare_query->fetchAll();
+	$n_res = count($reponse);
+}
+catch (Exception $e)
+{
+    echo 'Échec lors de la connexion : ' . $e->getMessage();
+}
+// on fait une boucle qui va faire un tour pour chaque enregistrement 
+if($n_res > 0)//$reponse->fetch()) 
+{
+	// on affiche les informations de l'enregistrement en cours 
+	echo '<p>Welcome brother</p>';
+}else{
+    echo '<p>Go back home dude</p>';
+}	
+$prepare_query->closeCursor();
+?>
+<h1>Next...</h1>
+<p><a href='../create.php'>Create a new account</a></p>
+
+
+</body>
+</html>
