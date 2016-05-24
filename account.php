5ea6c2544a70e3b71523ebb64ec2718e62ba9c69
BThierry <bertand.thierry1@gmail.com>
2016-05-24 14:29:23 +0200

    admin menu

--- account.php
+++ account.php
@@ -8,6 +8,7 @@
 <?php
 $sha_url = "";
 empty($_GET['sha']) ? $sha_url = "" : $sha_url = htmlspecialchars($_GET['sha']);
+$admin_mode = false;
 ?>
 
 <?php 
@@ -34,6 +35,31 @@ catch (Exception $e)
 {
     echo 'Échec lors de la connexion : ' . $e->getMessage();
 }
+
+//Check if admin
+$admin_mode_url = false;
+empty($_GET['admin']) ? $admin_mode_url = false : $admin_mode_url = true;
+if($admin_mode_url)
+	{//Let's check that it's not a trap
+	try
+	{
+		$myquery_admin = 'SELECT * FROM accounts WHERE hashid_admin = :sha_url';
+		$prepare_query_admin = $db->prepare($myquery);
+		$prepare_query_admin->bindValue(':sha_url', $sha_url, PDO::PARAM_STR);
+		$prepare_query_admin->execute();
+		$reponse_admin = $prepare_query_admin->fetchAll();
+	}
+	catch (Exception $e)
+	{
+		echo 'Échec lors de la connexion : ' . $e->getMessage();
+	}
+	$n_res_admin = count($reponse_admin);
+	if($n_res_admin > 0)
+	{
+		$admin_mode = true;
+	}
+}
+
 // on fait une boucle qui va faire un tour pour chaque enregistrement 
 if($n_res > 0)//$reponse->fetch()) 
 {
@@ -46,7 +72,17 @@ if($n_res > 0)//$reponse->fetch())
 }else{
 	echo '<h1>WRONG ACCOUNT</h1>';
 }	
+
+//If admin mode
+if($admin_mode)
+{
+echo '	<h1>Administration section</h1>';	
+}
 $prepare_query->closeCursor();
+if($admin_mode_url)
+{
+	$prepare_query_admin->closeCursor();
+}
 ?>
 <h1>Menu</h1>
 <ul>
