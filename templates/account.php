c503ababb3fdd540f5e81041f3523b6652233166
BThierry <bertand.thierry1@gmail.com>
2016-05-25 18:06:28 +0200

    javascript account for "smart" dropdown

--- templates/account.php
+++ templates/account.php
@@ -2,25 +2,7 @@
 
 <html>
 <head>
-<script type="text/javascript">
-function configureDropDownLists(payer, receiver) {
-    var choice1 = payer.value;
-	receiver.length= 0;
-	createOption(receiver, 'Group', -1);
-	for (i = 0; i < payer.options.length; i++) {
-		if(payer.value != payer.options[i].value && payer.options[i].value != 'null')
-		{
-			createOption(receiver, payer.options[i].text, payer.options[i].value);
-		}
-	}
-}
-
-    function createOption(ddl, text, value) {
-        var opt = document.createElement('option');
-        opt.value = value;
-        opt.text = text;
-        ddl.options.add(opt);
-    }
+<script type="text/javascript" src="/DivideTheBill/js/account.js">
 </script>
 </head>
 <body>
