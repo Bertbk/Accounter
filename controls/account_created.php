e5176842fa85fe98e69dcd98bb05652fb957b8ce
BThierry <bertand.thierry1@gmail.com>
2016-06-08 14:26:26 +0200

    cleaning account_created.php

--- controls/account_created.php
+++ controls/account_created.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/../config-app.php';
 include_once(LIBPATH.'/accounts/get_account.php');
 include_once(LIBPATH.'/accounts/get_account_admin.php');
 
-$isdone = true;
+include_once(LIBPATH.'/hashid/validate_hashid.php');
 
 //Get Hashid
 $hashid_url = "";
@@ -16,26 +16,27 @@ $hashid = $hashid_url;
 $hashid_admin = $hashid_admin_url;
 
 //If empty, go back home.
-if($hashid_url == "" || strlen($hashid_url) != 16 || $hashid_admin_url == "" || strlen($hashid_admin_url) != 32 )
+if(!validate_hashid($hashid)
+	|| !validate_hashid_admin($hashid_admin)
+	)
 {
-	$isdone = false;
 	header ('location:'.BASEURL);
 }
 
+//check if the hashids link to the right account (same and existant)
 $my_account = array();
 $my_account = get_account($hashid);
+$my_account_admin = array();
 $my_account_admin = get_account_admin($hashid_admin);
 
-if(empty($my_account) ||empty($my_account_admin))
+if(empty($my_account) 
+	|| empty($my_account_admin)
+  || $my_account['id'] !== $my_account_admin['id'])
 {
-	$isdone = false;
 	header ('location:'.BASEURL);
 }
 
-if($isdone)
-{
-	$link_contrib = BASEURL.'/account/'.$hashid;
-	$link_admin = BASEURL.'/account/'.$hashid_admin.'/admin';	
-}	
+$link_contrib = BASEURL.'/account/'.$hashid;
+$link_admin = BASEURL.'/account/'.$hashid_admin.'/admin';	
+
 include_once(ABSPATH.'/templates/account_created.php');
-?>
--- templates/account_created.php
+++ templates/account_created.php
@@ -5,18 +5,14 @@
 </head>
 <body>
 
-<?php 
-if($isdone)
-{
-?>
-	<h1> Accoung created</h1>
-	<ul>
-	<li>Contributor link  : <a href="<?php echo $link_contrib?>"><?php echo $link_contrib?> </a></li>
-	<li>Administrator link: <a href="<?php echo $link_admin?>"><?php echo $link_admin?> </a> </li>
-	</ul>
-<?php
-}
-?>
+
+
+<h1> Accoung created</h1>
+<ul>
+<li>Contributor link  : <a href="<?php echo $link_contrib?>"><?php echo $link_contrib?> </a></li>
+<li>Administrator link: <a href="<?php echo $link_admin?>"><?php echo $link_admin?> </a> </li>
+</ul>
+
 
 </body>
 </html>
