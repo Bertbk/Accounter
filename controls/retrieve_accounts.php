c34b9eeeec30da687d0f2c6a3c5a64816dd03392
BThierry <bertand.thierry1@gmail.com>
2016-06-08 14:09:06 +0200

    clean recover accounts

--- controls/retrieve_accounts.php
+++ controls/retrieve_accounts.php
@@ -1,19 +1,53 @@
 <?php 
 require_once __DIR__.'/../config-app.php';
 
-$problem = 0;
-empty($_GET['pb']) ? $problem = 0 : $problem = (int)$_GET['pb'];
+$errorMessage = array(
+'no_account' => 'Sorry, no account associated with this email address has been found. 
+	<br> Please try again with another email address',
+'email_not_sent' => 'Sorry, the email couldn\'t be sent',
+'email_field_empty' => 'Please fill the email field',
+'email_field_not_valid' => 'Email field is not valid'	
+);
 
 $errArray = array();
+//GET ACCOUNTS BY EMAIL ADDRESS
+$ArrayOfAccounts = array();
+$is_sent = false;
+if(isset($_POST['submit_email']))
+{
+	$p_email = null;
+	if(empty($_POST['p_email']))
+	{
+		array_push($errArray, $errorMessage['no_account']);
+	}
+	if(filter_input(INPUT_POST, 'p_email', FILTER_VALIDATE_EMAIL) === false)
+	{
+		array_push($errArray, $errorMessage['email_field_not_valid']);
+	}
+	$p_email = filter_input(INPUT_POST, 'p_email', FILTER_SANITIZE_EMAIL);
+	$ArrayOfAccounts = get_accounts_by_email($p_email);
+	//Send to email
+	if(!empty($ArrayOfAccounts))
+	{
+		$is_sent = send_email_with_accounts($p_email, $ArrayOfAccounts);
+		if($is_sent)
+		{
+			header('location: '.BASEURL.'/accounts_sent.php');
+		}
+		else
+		{
+		array_push($errArray, $errorMessage['email_not_sent']);
+		}
+	}
+	else
+	{
+		array_push($errArray, $errorMessage['no_account']);
+	}
+}
+else
+{
+		array_push($errArray, $errorMessage['email_field_empty']);
+}
 
-if($problem == 1)
-{
-	array_push($errArray, 'Sorry, no account associated with this email address has been found. 
-	<br> Please try again with another email address');
-}
-else if($problem == 2)
-{
-		array_push($errArray, 'Sorry, the email couldn\'t be sent');
-}
 
 include_once(ABSPATH.'/templates/retrieve_accounts.php');
