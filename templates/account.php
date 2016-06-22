b972c06e11883fac0dde9d617a45110a97d37c7d
BThierry <bertand.thierry1@gmail.com>
2016-06-22 13:23:33 +0200

    fix date picker jquery (now working!)

--- templates/account.php
+++ templates/account.php
@@ -9,10 +9,11 @@
 <link rel="stylesheet" type="text/css" href="<?php echo BASEURL.'/css/bill_participant.css'?>">
 <link rel="stylesheet" type="text/css" href="<?php echo BASEURL.'/css/account.css'?>">
 
-<script type="text/javascript" src="<?php echo BASEURL.'/js/account.js'?>"></script>
 <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/smoothness/jquery-ui.css" media="all">
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="<?php echo BASEURL.'/js/account.js'?>"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo BASEURL.'/js/hide_show_add_participant.jquery'?>"></script>
 
 </head>
