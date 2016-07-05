3b8e12b7958b4d83ec956a995aba48e039ce5b33
BThierry <bertand.thierry1@gmail.com>
2016-07-05 15:01:03 +0200

    css admin page

--- admin/index.php
+++ admin/index.php
@@ -61,80 +61,82 @@ $accounts = get_accounts();
 	<?php include(__DIR__.'/../templates/messages/messages.php');?>
 
 			<h1>Table of the accounts</h1>
-			<div class="row">
-				<div class="hidden-xs hidden-sm hidden-md col-lg-2 ">
-					<strong>Title</strong>
-				</div>
-				<div class="hidden-xs hidden-sm hidden-md col-lg-1 ">
-					<strong>Author</strong>
-				</div>
-				<div class="hidden-xs hidden-sm hidden-md col-lg-2 ">
-					<strong>Email</strong>
-				</div>
-				<div class="hidden-xs hidden-sm hidden-md col-lg-2 ">
-					<strong>Description</strong>
-				</div>
-				<div class="hidden-xs hidden-sm hidden-md col-lg-1 ">
-					<strong>Expiration date</strong>
-				</div>
-				<div class="hidden-xs hidden-sm col-md-2 col-lg-1 ">
-					<strong>Admin Link</strong>
-				</div>
-				<div class="hidden-xs hidden-sm col-md-2 col-lg-1 ">
-					<strong>Delete</strong>
-				</div>
-			</div>
-		<?php 
-		$cpt_account = -1;
-		$evenly = "even";
-		foreach ($accounts as $account)
-		{
-			$cpt_account ++;
-			$evenly = ($evenly == "even")?"uneven":"even";
-		?>
-			<div class="<?php 'account_'.$cpt_account?>">
-				<div class="row <?php echo 'row_'.$evenly?>">
-					<div class="col-xs-12 col-lg-2">
-						<?php echo htmlspecialchars($account['title'])?>	
+			<div id="table_of_accounts">
+				<div class="row ">
+					<div class="hidden-xs hidden-sm col-md-3 col-lg-3 ">
+						<strong>Title</strong>
 					</div>
-					<div class="hidden-xs hidden-sm hidden-md col-lg-1 <?php echo 'admin_table_collapse_'.$cpt_account?>">
-						<?php echo htmlspecialchars($account['author'])?>	
+					<div class="hidden-xs hidden-sm col-md-2 col-lg-1 ">
+						<strong>Author</strong>
 					</div>
-					<div class="hidden-xs hidden-sm hidden-md col-lg-2 <?php echo 'admin_table_collapse_'.$cpt_account?>">
-						<?php echo htmlspecialchars($account['email'])?>	
+					<div class="hidden-xs hidden-sm col-md-2 col-lg-2 ">
+						<strong>Email</strong>
 					</div>
-					<div class="hidden-xs hidden-sm hidden-md col-lg-2 <?php echo 'admin_table_collapse_'.$cpt_account?>">
-						<?php echo htmlspecialchars($account['description'])?>	
+					<div class="hidden-xs hidden-sm col-md-3 col-lg-3 ">
+						<strong>Description</strong>
 					</div>
-					<div class="hidden-xs hidden-sm hidden-md col-lg-1 <?php echo 'admin_table_collapse_'.$cpt_account?>">
-						<?php echo htmlspecialchars($account['date_of_expiration'])?>	
+					<div class="hidden-xs hidden-sm col-md-2 col-lg-1 ">
+						<strong>Expiration date</strong>
 					</div>
-					<div class="col-xs-2 col-lg-1">
-						<a class="btn btn-default" href="<?php echo BASEURL.'/account/'.$account['hashid_admin'].'/admin'?>"><span class="glyphicon glyphicon-link"></span></a>	
+					<div class="hidden-xs hidden-sm hidden-md col-lg-1 ">
+						<strong>Admin Link</strong>
 					</div>
-					<div class="col-xs-2 col-lg-1">
-						<form method="post" action="<?php echo ACTIONPATH.'/delete_account.php'?>">
-							<input type="hidden" name="p_hashid_account" 
-								value="<?php echo $account['hashid_admin']?>">
-							<input type="hidden" name="p_redirect" 
-								value="admin_page">
-							<button type="submit" class="btn btn-default confirmation" name="submit_delete_account">
-								<span class="glyphicon glyphicon-trash"></span>
+					<div class="hidden-xs hidden-sm hidden-md col-lg-1 ">
+						<strong>Delete</strong>
+					</div>
+				</div>
+			<?php 
+			$cpt_account = -1;
+			$evenly = "even";
+			foreach ($accounts as $account)
+			{
+				$cpt_account ++;
+				$evenly = ($evenly == "even")?"uneven":"even";
+			?>
+				<div class="<?php 'account_'.$cpt_account?>">
+					<div class="row <?php echo 'row_'.$evenly?>" >
+						<div class="col-xs-12 col-md-3 col-lg-3">
+							<?php echo htmlspecialchars($account['title'])?>	
+						</div>
+						<div class="hidden-xs hidden-sm col-md-2 col-lg-1 <?php echo 'admin_table_collapse_'.$cpt_account?>">
+							<?php echo htmlspecialchars($account['author'])?>	
+						</div>
+						<div class="hidden-xs hidden-sm col-md-2 col-lg-2 <?php echo 'admin_table_collapse_'.$cpt_account?>">
+							<?php echo htmlspecialchars($account['email'])?>	
+						</div>
+						<div class="hidden-xs hidden-sm col-md-3 col-lg-3 <?php echo 'admin_table_collapse_'.$cpt_account?>">
+							<?php echo htmlspecialchars($account['description'])?>	
+						</div>
+						<div class="hidden-xs hidden-sm col-md-2 col-lg-1 <?php echo 'admin_table_collapse_'.$cpt_account?>">
+							<?php echo htmlspecialchars($account['date_of_expiration'])?>	
+						</div>
+						<div class="col-xs-2 col-md-1">
+							<a class="btn btn-default" href="<?php echo BASEURL.'/account/'.$account['hashid_admin'].'/admin'?>"><span class="glyphicon glyphicon-link"></span></a>	
+						</div>
+						<div class="col-xs-2 col-md-1">
+							<form method="post" action="<?php echo ACTIONPATH.'/delete_account.php'?>">
+								<input type="hidden" name="p_hashid_account" 
+									value="<?php echo $account['hashid_admin']?>">
+								<input type="hidden" name="p_redirect" 
+									value="admin_page">
+								<button type="submit" class="btn btn-default confirmation" name="submit_delete_account">
+									<span class="glyphicon glyphicon-trash"></span>
+								</button>
+							</form>
+						</div>
+						
+						
+				<?php //Collapse button (for mobile>) ?>
+						<div class="visible-xs visible-sm col-xs-2">
+							<button type="submit" class="btn btn-default"
+								data-toggle="collapse" data-target=".<?php echo 'admin_table_collapse_'.$cpt_account?>">
+								<span class="glyphicon glyphicon-plus"></span>
 							</button>
-						</form>
-					</div>
-					
-					
-			<?php //Collapse button (for mobile>) ?>
-					<div class="visible-xs visible-sm col-xs-2">
-						<button type="submit" class="btn btn-default"
-							data-toggle="collapse" data-target=".<?php echo 'admin_table_collapse_'.$cpt_account?>">
-							<span class="glyphicon glyphicon-plus"></span>
-						</button>
+						</div>
 					</div>
 				</div>
+	<?php } ?>
 			</div>
-<?php } ?>
 		</div>
 	</div> <!-- content -->
 </body>
